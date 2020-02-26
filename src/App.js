62bef63e7441e5474251fd1ce48048693d151a61
Ossian Petri <ossian.p@gmail.com>
2020-02-26 16:06:06 +0100

    Add Menu buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Menu from './Utils/Menu';
 import SkillPage from './Skills/SkillPage';
 
 function App() {
   return (
     <div className="background">
       <div className="page-root-menu">
-
+        <Menu />        
       </div>
       <div className="page-background">
         <SkillPage />
---
+++ src/Utils/Menu.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './Menu.css'
+
+const buttonActive = "menu-button-content-selected";
+const buttonInactive = "menu-button-content";
+
+class Menu extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { activeButton: 0 };
+    }
+
+    render() {
+        return (
+            <div className="menu-box">
+                <div className="menu-box-left">
+                    <div>BUTTONS GOES HERE</div>
+                </div>
+                <div className="menu-box-center">
+                    <div onClick={() => this.setState({ activeButton: 0 })} className="menu-button">
+                        <div className={this.state.activeButton == 0 ? buttonActive : buttonInactive}>
+                            Character
+                        </div>
+                    </div>
+                    <div onClick={() => this.setState({ activeButton: 1 })} className="menu-button">
+                        <div className={this.state.activeButton == 1 ? buttonActive : buttonInactive}>
+                            Skills
+                        </div>
+                    </div>
+                    <div onClick={() => this.setState({ activeButton: 2 })} className="menu-button">
+                        <div className={this.state.activeButton == 2 ? buttonActive : buttonInactive}>
+                            Combat
+                        </div>
+                    </div>
+                    <div onClick={() => this.setState({ activeButton: 3 })} className="menu-button">
+                        <div className={this.state.activeButton == 3 ? buttonActive : buttonInactive}>
+                            Inventory
+                        </div>
+                    </div>
+                    <div onClick={() => this.setState({ activeButton: 4 })} className="menu-button">
+                        <div className={this.state.activeButton == 4 ? buttonActive : buttonInactive}>
+                            Notes
+                        </div>
+                    </div>
+                    <div onClick={() => this.setState({ activeButton: 5 })} className="menu-button">
+                        <div className={this.state.activeButton == 5 ? buttonActive : buttonInactive}>
+                            Talent Trees
+                        </div>
+                    </div>
+                </div>
+                <div className="menu-box-right">
+                    <div>BUTTONS GOES HERE</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Menu;
